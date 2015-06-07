a2eef5cae185ea0576c4b96f2c6438d2653606bb
Richard Russon <rich@flatcap.org>
2015-06-07 01:40:11 +0100

    generalise scripts

---
+++ dnsmasq-hosts/dnsmasq-hosts.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use English qw(-no_match_vars);
+use JSON;
+
+our $VERSION = 0.1;
+
+sub generate
+{
+	my ($filename) = @_;
+
+	my $json_text = q{};
+	if (open FH, '<', $filename) {
+		local $INPUT_RECORD_SEPARATOR = undef;
+		$json_text = <FH>;
+		close FH;
+	}
+
+	# Trim any whitespace
+	$json_text =~ s/^\s+|\s+$//g;
+
+	# printf "# DNS for VM hosts using DHCP\n";
+	if ($json_text ne q{}) {
+		my $json = JSON->new;
+		my $data = $json->decode ($json_text);
+
+		foreach my $row (@{$data}) {
+			# printf "%-18s%-12s%15s%30s # %s\n", $row->{'ip-address'}, $row->{'hostname'}, $row->{'hostname'} . '.vm', $row->{'hostname'} . '.vm.flatcap.org', $row->{'mac-address'};
+			printf "%-18s%-12s\n", $row->{'ip-address'}, $row->{'hostname'};
+		}
+	}
+	# printf "\n";
+}
+
+
+foreach (@ARGV) {
+	generate ($_);
+}
+
---
+++ dnsmasq-hosts/dnsmasq-hosts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PATH="/usr/bin:/usr/sbin"
+
+set -o errexit	# set -e
+set -o nounset	# set -u
+
+shopt -s nullglob
+
+renice --priority 19 --pid $$ > /dev/null
+ionice --class 3     --pid $$ > /dev/null
+
+umask 0022
+
+DIR="/var/lib/libvirt/dnsmasq"
+
+cd "$DIR"
+
+for FILE in virbr*; do
+	./dnsmasq-hosts.pl "$FILE"
+done > hosts
+
